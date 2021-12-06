d470d93331bb9aee38302e6f54790f299981f41a
pete hotchkiss <pete.hotchkiss@gmail.com>
2021-12-06 15:19:58 +0000

    🚧 #3: create 3 char flag file from match to 2 char

--- scripts/2-to-3.js
+++ scripts/2-to-3.js
@@ -1,1 +1,33 @@
 const codes = require("../data/iso-country-codes.json");
+const fs = require('fs');
+
+console.log(codes.data.length);
+
+const create = async () => {
+  let baseFolder = "./flags/";
+
+  //  iterate over a list of files in the folder
+  fs.readdirSync(`${baseFolder}2 char`)
+  .forEach( async file => {
+    let twoCharCode = file.split(".")[0];
+
+    if (file.indexOf("svg") !== -1 ) {
+      let country = codes.data.find(i => i["alpha-2"] === twoCharCode.toUpperCase());
+
+      if (country) {
+        //  theres been a match for the 2 char code
+        await fs.copyFileSync(
+          `${baseFolder}2 char/${file}`,
+          `${baseFolder}3 char/${country["alpha-3"].toLowerCase()}.svg`,
+        )
+      }
+    }
+
+  })
+}
+
+const run = async() => {
+  await create();
+}
+
+run();
